7398107f39fa4989139634267e2efba5a43d73cd
Κωνσταντίνος Αρβανιτάκης < >
2018-01-22 22:25:27 +0100

    Add project files.

---
+++ ReactMvcPlayground/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ReactMvcPlayground
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
